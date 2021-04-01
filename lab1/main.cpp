4d79d1b25580ed6a51973504d4d45287ce510ed1
Luke2336 <lukeyu.eecs07@nctu.edu.tw>
2021-04-01 22:58:07 +0800

    update main

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -4,43 +4,33 @@ using namespace std;
 vector<vector<int>> Nets;
 vector<vector<int>> Cells;
 
-inline void genRandomBit(vector<bool> &v) {
-  fill(v.begin(), v.begin() + (v.size() >> 1), 1);
-  unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-  shuffle(v.begin(), v.end(), std::default_random_engine(seed));
-}
-
-inline bool range(int v, int L, int R) { return L <= v && v <= R; }
-
 class BucketList {
-  vector<int> Bucket[2];
   vector<int> Next, Pre;
-  int Mx[2];
-  int P, n;
-  int node_id(int v) { return (Bucket[0].size() << 1) + v; }
-  int bucket_id(int b, int id) { return Bucket[0].size() * b + id; }
+  int P, maxP, n, Mx[2];
+  int node_id(int v) { return (maxP << 1) + v; }
+  int node_reid(int i) { return i - (maxP << 1); }
+  int bucket_id(int b, int id) { return b ? maxP + id : id; }
 
 public:
-  BucketList(int P, int n) : P(P), n(n) {
-    Mx[0] = Mx[1] = 0;
-    int sz = P << 1 | 1;
-    Bucket[0].resize(sz, -1), Bucket[1].resize(sz, -1);
-    Next.resize(node_id(n)), Pre.resize(node_id(n));
-    for (int i = 0; i < 2; ++i)
-      for (int j = 0; j < sz; ++j)
-        Bucket[i][j] = bucket_id(i, j);
+  BucketList(int P, int n) : P(P), n(n), maxP((P << 1) | 1) {
+    Mx[0] = Mx[1] = -1;
+    Next.resize((maxP << 1) + n, -1);
+    Pre.resize(Next.size(), -1);
   }
-  int mx(int b) { return Mx[b]; }
+  int mx(int b) {
+    while (Mx[b] && Next[bucket_id(b, Mx[b])] == -1)
+      --Mx[b];
+    return Mx[b];
+  }
   int pop(int b) {
-    int bd = Mx[b];
+    int bd = bucket_id(b, mx(b));
     int nd = Next[bd];
+    if (nd < 0) return -1;
     Next[bd] = Next[nd];
     if (Next[nd] != -1)
       Pre[Next[nd]] = bd;
-    while (Next[bucket_id(b, Mx[b])] == -1)
-      --Mx[b];
     Pre[nd] = Next[nd] = -1;
-    return nd;
+    return node_reid(nd);
   }
   void remove(int v) {
     int nd = node_id(v);
@@ -59,8 +49,8 @@ public:
     Mx[b] = max(Mx[b], id);
   }
 };
-
-int FM(vector<bool> Partition, const int P) {
+inline bool range(int v, int L, int R) { return L <= v && v <= R; }
+int FM(vector<bool> &Partition, const int P) {
   int N = Cells.size(), M = Nets.size();
   int M1 = ceil(N * 0.45), M2 = floor(N * 0.55);
   vector<bool> Lock(N);
@@ -82,11 +72,17 @@ int FM(vector<bool> Partition, const int P) {
   vector<int> swapNode;
   int sz[2] = {N >> 1, N - (N >> 1)};
   for (int t = 0; t < N; ++t) {
-    bool b = range(sz[1] - 1, M1, M2) && BL.mx(0) < BL.mx(1) ||
-             !range(sz[0] - 1, M1, M2);
+    bool b;
+    if (range(sz[0] - 1, M1, M2) && (sz[1] < 0 || BL.mx(0) >= BL.mx(1)))
+      b = 0;
+    else if (range(sz[0] + 1, M1, M2) && (sz[0] < 0 || BL.mx(0) <= BL.mx(1)))
+      b = 1;
+    else
+      break;
     --sz[b], ++sz[!b];
     sumGain.push_back(BL.mx(b) - P);
     int node = BL.pop(b);
+    if (node < 0) break;
     Lock[node] = true;
     swapNode.push_back(node);
     for (int i : Cells[node]) {
@@ -96,36 +92,47 @@ int FM(vector<bool> Partition, const int P) {
           continue;
         if (!CntCell[b][i] && Partition[v] != b) {
           BL.remove(v);
-          BL.add(v, !b, --Gain[v] + P);
+          BL.add(v, !b, (--Gain[v]) + P);
         } else if (CntCell[b][i] == 1 && Partition[v] == b) {
           BL.remove(v);
-          BL.add(v, b, ++Gain[v] + P);
+          BL.add(v, b, (++Gain[v]) + P);
         }
       }
     }
   }
   int mx_i = 0;
-  for (int i = 1; i < sumGain.size(); i++) {
+  for (int i = 1; i < sumGain.size(); ++i) {
     sumGain[i] += sumGain[i - 1];
     if (sumGain[i] > sumGain[mx_i])
       mx_i = i;
   }
-  if (sumGain[mx_i] <= 0)
-    return 0;
-  for (int i = 0; i <= mx_i; i++)
-    Partition[swapNode[i]] = !Partition[swapNode[i]];
-  return sumGain[mx_i];
+  if (sumGain.size() && sumGain[mx_i] > 0)
+    for (int i = 0; i <= mx_i; ++i)
+      Partition[swapNode[i]] = !Partition[swapNode[i]];
+  int ret = 0;
+  for (int i = 0; i < M; ++i) {
+    bool cut = false;
+    for (int v : Nets[i])
+      if (Partition[v] != Partition[Nets[i][0]])
+        cut = true;
+    ret += cut;
+  }
+  return ret;
 }
-
+inline void genRandomBit(vector<bool> &v) {
+  fill(v.begin(), v.begin() + (v.size() >> 1), 1);
+  unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+  shuffle(v.begin(), v.end(), std::default_random_engine(seed));
+}
 int main(int argv, char *argc[]) {
   freopen(argc[1], "r", stdin);
-  // freopen("output.txt", "w", stdout);
+  freopen("output.txt", "w", stdout);
   ios::sync_with_stdio(0), cin.tie(0);
   srand(time(0));
   int N, M, P = 0;
   cin >> M >> N;
   Nets.resize(M), Cells.resize(N);
-  for (int i = 0; i < M; i++) {
+  for (int i = 0; i < M; ++i) {
     string s;
     getline(cin, s);
     stringstream ss;
@@ -137,17 +144,17 @@ int main(int argv, char *argc[]) {
   // int run = max(1, (int)2e9 / P);
   int run = 1;
   vector<vector<bool>> Partition(run, vector<bool>(N));
-  vector<int> CntCut;
+  vector<int> CntCut(run);
 #pragma omp parallel for
-  for (int i = 0; i < run; i++) {
+  for (int i = 0; i < run; ++i) {
     genRandomBit(Partition[i]);
     CntCut[i] = FM(Partition[i], P);
   }
   int ans = 0;
-  for (int i = 1; i < run; i++)
+  for (int i = 1; i < run; ++i)
     if (CntCut[i] < CntCut[ans])
       ans = i;
-  for (int i = 0; i < N; i++)
+  for (int i = 0; i < N; ++i)
     cout << Partition[ans][i] << '\n';
   return 0;
 }
