6173b9844f8643f0301bcc9e15fa2026a7867d54
Luke2336 <lukeyu.eecs07@nctu.edu.tw>
2021-06-16 15:16:27 +0800

    test OK

--- lab4/KF-ATPG/readbench.tab.h
+++ lab4/KF-ATPG/readbench.tab.h
@@ -1,14 +1,13 @@
-/* A Bison parser, made by GNU Bison 2.3.  */
+/* A Bison parser, made by GNU Bison 3.0.4.  */
 
-/* Skeleton interface for Bison's Yacc-like parsers in C
+/* Bison interface for Yacc-like parsers in C
 
-   Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
-   Free Software Foundation, Inc.
+   Copyright (C) 1984, 1989-1990, 2000-2015 Free Software Foundation, Inc.
 
-   This program is free software; you can redistribute it and/or modify
+   This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2, or (at your option)
-   any later version.
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
 
    This program is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -16,9 +15,7 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 51 Franklin Street, Fifth Floor,
-   Boston, MA 02110-1301, USA.  */
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
 
 /* As a special exception, you may create a larger work that contains
    part or all of the Bison parser skeleton and distribute that work
@@ -33,66 +30,62 @@
    This special exception was added by the Free Software Foundation in
    version 2.2 of Bison.  */
 
-/* Tokens.  */
+#ifndef YY_KK_READBENCH_TAB_H_INCLUDED
+# define YY_KK_READBENCH_TAB_H_INCLUDED
+/* Debug traces.  */
+#ifndef YYDEBUG
+# define YYDEBUG 0
+#endif
+#if YYDEBUG
+extern int kkdebug;
+#endif
+
+/* Token type.  */
 #ifndef YYTOKENTYPE
 # define YYTOKENTYPE
-   /* Put the tokens into the symbol table, so that GDB and other debuggers
-      know about them.  */
-   enum yytokentype {
-     GINPUT = 258,
-     GOUTPUT = 259,
-     GDFF = 260,
-     GNOT = 261,
-     GAND = 262,
-     GOR = 263,
-     GNAND = 264,
-     GNOR = 265,
-     GBUF = 266,
-     GXOR = 267,
-     EOLINE = 268,
-     LPAR = 269,
-     RPAR = 270,
-     COMMA = 271,
-     EQUAL = 272,
-     NAMESTRING = 273
-   };
+  enum yytokentype
+  {
+    GINPUT = 258,
+    GOUTPUT = 259,
+    GDFF = 260,
+    GNOT = 261,
+    GAND = 262,
+    GOR = 263,
+    GNAND = 264,
+    GNOR = 265,
+    GBUF = 266,
+    GXOR = 267,
+    EOLINE = 268,
+    LPAR = 269,
+    RPAR = 270,
+    COMMA = 271,
+    EQUAL = 272,
+    NAMESTRING = 273
+  };
 #endif
-/* Tokens.  */
-#define GINPUT 258
-#define GOUTPUT 259
-#define GDFF 260
-#define GNOT 261
-#define GAND 262
-#define GOR 263
-#define GNAND 264
-#define GNOR 265
-#define GBUF 266
-#define GXOR 267
-#define EOLINE 268
-#define LPAR 269
-#define RPAR 270
-#define COMMA 271
-#define EQUAL 272
-#define NAMESTRING 273
 
-
-
-
+/* Value type.  */
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
-typedef union YYSTYPE
-#line 31 "readbench.y"
+
+union YYSTYPE
 {
+#line 31 "readbench.y" /* yacc.c:1909  */
+
 long num;
 char str[256];
 GATEFUNC func;
-}
-/* Line 1529 of yacc.c.  */
-#line 91 "readbench.tab.h"
-	YYSTYPE;
-# define yystype YYSTYPE /* obsolescent; will be withdrawn */
-# define YYSTYPE_IS_DECLARED 1
+
+#line 79 "readbench.tab.h" /* yacc.c:1909  */
+};
+
+typedef union YYSTYPE YYSTYPE;
 # define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
 #endif
+
 
 extern YYSTYPE kklval;
 
+int kkparse (void);
+
+#endif /* !YY_KK_READBENCH_TAB_H_INCLUDED  */
